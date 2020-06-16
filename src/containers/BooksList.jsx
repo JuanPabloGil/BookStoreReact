2ce6631483c8d1a239efe11fc40f006a5a1fad9e
Juan Pablo <jpablomgil@gmail.com>
2020-06-16 11:07:01 -0500

    Create method to select wich category are going to display

--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -1,52 +1,57 @@
 import React from 'react';
-import {useSelector} from 'react-redux'
-import {useDispatch} from 'react-redux'
-import {removeBook} from '../actions'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { removeBook } from '../actions'
 import Book from '../components/Book'
 import CategoryFilter from '../components/CategoryFilter'
 
 
 const BooksList = () => {
-  const books = useSelector ( state => state.books);
-  const filter = useSelector ( state => state.filter);
+  const books = useSelector(state => state.books);
+  const filter = useSelector(state => state.filter);
 
   const dispatch = useDispatch();
 
   const handleRemoveBook = (id) => {
     dispatch(removeBook(id));
   }
-  let currentBooks = []
-  if (filter === '') {
-    currentBooks = books.filter(book => book.category !== filter);
-  }else {
-    currentBooks = books.filter(book => book.category === filter);
-  }
+
  
+  const filterByCategory = () => {
+    let currentBooks = []
+    if (filter === '') {
+      currentBooks = books.filter(book => book.category !== filter);
+    } else {
+      currentBooks = books.filter(book => book.category === filter);
+    }
+    return currentBooks
+  }
+
   return (
     <div>
       <h1>BooksList</h1>
-        <div>
-          <CategoryFilter />
-        </div>
+      <div>
+        <CategoryFilter />
+      </div>
 
       <table>
         <thead>
           <tr>
-          <th>Title</th>
-          <th>Category</th>
-          <th>ID</th>
-        </tr>  
+            <th>Title</th>
+            <th>Category</th>
+            <th>ID</th>
+          </tr>
         </thead>
-        
+
         <tbody>
-          {currentBooks.map((book) => <Book key={book.id} book={book} handleRemoveBook = { handleRemoveBook }/>)}
+          {filterByCategory().map((book) => <Book key={book.id} book={book} handleRemoveBook={handleRemoveBook} />)}
         </tbody>
-        
-        
+
+
       </table>
     </div>
-    
-    );
+
+  );
 }
- 
+
 export default BooksList;
